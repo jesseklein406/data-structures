93f936fab370339a121591cdbd1d770ba817437b
Muoi Ly <muoi.ly@gmail.com>
2015-06-24 16:18:31 -0700

    added search, remove, display methods to linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -6,41 +6,57 @@ class Node(object):
 
 
 class LinkedList(object):
-    size = 0
-    head = None
+    
+    def __init__(self):
+        self.sizeOfList = 0
+        self.head = None
 
     def insert(self, val):
         new_node = Node(val)
         new_node.next_node = self.head
         self.head = new_node
+        self.sizeOfList += 1
         print "head is now", self.head.value
 
-    def display(self):
-        pass
-        #return self.displayList
-    
     def pop(self):
         val = self.head.value
         self.head = self.head.next_node
+        self.sizeOfList -= 1
         return val
 
-    #def size():
+    def size(self):
+        return self.sizeOfList
 
-    #def search(val):
+    def search(self,val):
+        current = self.head
+
+        while current != None:
+            if current.value == val:
+                return current
+            current = current.next_node
+
+    def remove(self,node):
+        previous = self.head
+        current = self.head
+
+        if current is node:
+            self.head = current.next_node
+            self.sizeOfList -= 1
+        else:
+            while current != None and not (current is node):
+                previous = current
+                current = current.next_node
+                
+            if current is node:
+                previous.next_node = current.next_node
+                self.sizeOfList -=1
 
 
+    def display(self):
+        current = self.head
+        result = ()
+        while current != None:
+            result += (current.value,) 
+            current = current.next_node
+        return result    
 
-l1 = LinkedList()
-l1.insert(5)
-print
-print
-l1.insert(7)
-l1.insert(9)
-l1.insert(10)
-l1.insert(33)
-
-print
-print
-
-print l1.pop()
-print l1.pop()
