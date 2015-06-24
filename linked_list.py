87a5528a14b9416dcf43920adbeea35e36e266ef
Muoi Ly <muoi.ly@gmail.com>
2015-06-24 14:30:21 -0700

    updated linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -1,29 +1,26 @@
 class Node(object):
-    value = None
-    next_node = None
-
 
     def __init__(self, value):
         self.value = value
         self.next_node = None
 
 
-class List1(object):
+class LinkedList(object):
+    displayList = []
     size = 0
     head = None
-    displayList = []
 
     def insert(self, val):
-        new_node = Node(val)
         self.displayList.insert(0, val)
+
+        new_node = Node(val)
         new_node.next_node = self.head
         self.head = new_node
         print "head is now", self.head.value
         print "displayList: ", self.displayList
 
-
     def display(self):
-        print self.head.value
+        return self.displayList
     
     def pop(self):
         val = self.head.value
@@ -39,11 +36,19 @@ class List1(object):
 
 
 
-l1 = List1()
+l1 = LinkedList()
 l1.insert(5)
+print
+print "display():", l1.display()
+print
 l1.insert(7)
 l1.insert(9)
 l1.insert(10)
 l1.insert(33)
+
+print
+print "display():", l1.display()
+print
+
 print l1.pop()
 print l1.pop()
