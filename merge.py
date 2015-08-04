3bd64ce56523afe1d9fde7688bbaf4e0a3ded0ca
Jesse Klein <jesse@Jesses-MacBook-Air.local>
2015-08-04 15:41:45 -0700

    Add tests

--- merge.py
+++ merge.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
 
 
 def merge_sort(lst):
@@ -32,3 +36,116 @@ def merge(left, right):
         sort.extend(right[r_index:])
 
     return sort
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+
+    def best_case(n):
+        return range(n)
+
+    def worst_case(n):
+        return range(n, 0, -1)
+
+    print
+    print "Time for best case of list of first 10 integers"
+    print Timer(
+        'merge_sort(best_case(10))',
+        'from __main__ import merge_sort, best_case'
+    ).timeit(10) / 10
+
+    print
+    print "Time for worst case of list of first 10 integers"
+    print Timer(
+        'merge_sort(worst_case(10))',
+        'from __main__ import merge_sort, worst_case'
+    ).timeit(10) / 10
+
+    print
+    print "Time for best case of list of first 100 integers"
+    print Timer(
+        'merge_sort(best_case(100))',
+        'from __main__ import merge_sort, best_case'
+    ).timeit(10) / 10
+
+    print
+    print "Time for worst case of list of first 100 integers"
+    print Timer(
+        'merge_sort(worst_case(100))',
+        'from __main__ import merge_sort, worst_case'
+    ).timeit(10) / 10
+
+    print
+    print "Time for best case of list of first 1000 integers"
+    print Timer(
+        'merge_sort(best_case(1000))',
+        'from __main__ import merge_sort, best_case'
+    ).timeit(10) / 10
+
+    print
+    print "Time for worst case of list of first 1000 integers"
+    print Timer(
+        'merge_sort(worst_case(1000))',
+        'from __main__ import merge_sort, worst_case'
+    ).timeit(10) / 10
+
+    print
+    print "Time for best case of list of first 2000 integers"
+    print Timer(
+        'merge_sort(best_case(2000))',
+        'from __main__ import merge_sort, best_case'
+    ).timeit(10) / 10
+
+    print
+    print "Time for worst case of list of first 2000 integers"
+    print Timer(
+        'merge_sort(worst_case(2000))',
+        'from __main__ import merge_sort, worst_case'
+    ).timeit(10) / 10
+
+    print
+    print "Time for best case of list of first 3000 integers"
+    print Timer(
+        'merge_sort(best_case(3000))',
+        'from __main__ import merge_sort, best_case'
+    ).timeit(10) / 10
+
+    print
+    print "Time for worst case of list of first 3000 integers"
+    print Timer(
+        'merge_sort(worst_case(3000))',
+        'from __main__ import merge_sort, worst_case'
+    ).timeit(10) / 10
+
+    print
+    print "Time for best case of list of first 4000 integers"
+    print Timer(
+        'merge_sort(best_case(4000))',
+        'from __main__ import merge_sort, best_case'
+    ).timeit(10) / 10
+
+    print
+    print "Time for worst case of list of first 4000 integers"
+    print Timer(
+        'merge_sort(worst_case(4000))',
+        'from __main__ import merge_sort, worst_case'
+    ).timeit(10) / 10
+
+    print
+    print "Time for best case of list of first 5000 integers"
+    print Timer(
+        'merge_sort(best_case(5000))',
+        'from __main__ import merge_sort, best_case'
+    ).timeit(10) / 10
+
+    print
+    print "Time for worst case of list of first 5000 integers"
+    print Timer(
+        'merge_sort(worst_case(5000))',
+        'from __main__ import merge_sort, worst_case'
+    ).timeit(10) / 10
+
+    print "These tests approximately model O(n) behavior for best case"
+    print "and O(n2) behavior for worst case, so insertion sort works well"
+    print "for all sets that are reasonably sorted, or small sets that are"
+    print "not very sorted, and not very well for large poorly sorted data"
